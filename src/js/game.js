f0de7cce92ea3f8d3de5135dd921a8524d4c133b
wglugla <gluglawojciech@gmail.com>
2019-02-20 00:08:00 +0100

    Install lodash

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,8 @@
-import {
-   createSnake,
-   snakeBody,
-   moveSnake,
-   changeDirection
-} from './snake';
+import _ from 'lodash';
 
-import {
-   createFood
-} from './food';
+import { createSnake, snakeBody, moveSnake, changeDirection } from './snake';
+
+import { createFood } from './food';
 
 const startButton = document.querySelector('.menu__start');
 
@@ -16,42 +11,36 @@ const ctx = canvas.getContext('2d');
 ctx.scale(20, 20);
 
 const drawGame = () => {
-   ctx.fillStyle = '#668041';
-   ctx.fillRect(0, 0, 25, 25);
-}
+	ctx.fillStyle = '#668041';
+	ctx.fillRect(0, 0, 25, 25);
+};
 
-const drawFood = ({
-   x,
-   y
-}) => {
-   ctx.fillStyle = '#1d1b1b';
-   ctx.fillRect(x, y, 1, 1);
-   ctx.strokeStyle = '#668051';
-   ctx.fillRect(x, y, 1, 1);
-}
+const drawFood = ({ x, y }) => {
+	ctx.fillStyle = '#1d1b1b';
+	ctx.fillRect(x, y, 1, 1);
+	ctx.strokeStyle = '#668051';
+	ctx.fillRect(x, y, 1, 1);
+};
 
-const drawPart = ({
-   x,
-   y
-}) => {
-   ctx.fillStyle = '#000000';
-   ctx.fillRect(x, y, 1, 1);
-}
+const drawPart = ({ x, y }) => {
+	ctx.fillStyle = '#000000';
+	ctx.fillRect(x, y, 1, 1);
+};
 
 let foodPosition = createFood(25);
 
 const runGame = () => {
-   drawGame();
-   drawFood(foodPosition);
-   moveSnake();
-   for (let part of snakeBody) {
-      drawPart(part);
-   }
-}
+	drawGame();
+	drawFood(foodPosition);
+	moveSnake();
+	for (let part of snakeBody) {
+		drawPart(part);
+	}
+};
 
 startButton.addEventListener('click', () => {
-   createSnake();
-   setInterval(runGame, 200);
+	createSnake();
+	setInterval(runGame, 200);
 });
 
 /*
@@ -60,34 +49,30 @@ startButton.addEventListener('click', () => {
    2 - left
    3 - up
 */
-const findNewDirection = key => {
-   let dir;
-   switch (key) {
-      case 37:
-         dir = 2;
-         break;
-      case 38:
-         dir = 3;
-         break;
-      case 39:
-         dir = 0;
-         break;
-      case 40:
-         dir = 1;
-         break;
-      default:
-         break;
-   }
-   console.log(dir);
-   return dir;
-}
+const findNewDirection = (key) => {
+	let dir;
+	switch (key) {
+		case 37:
+			dir = 2;
+			break;
+		case 38:
+			dir = 3;
+			break;
+		case 39:
+			dir = 0;
+			break;
+		case 40:
+			dir = 1;
+			break;
+		default:
+			break;
+	}
+	console.log(dir);
+	return dir;
+};
 
 document.addEventListener('keydown', () => {
-   changeDirection(findNewDirection(window.event.keyCode));
-})
+	changeDirection(findNewDirection(window.event.keyCode));
+});
 
-
-export {
-   canvas,
-   ctx
-};
+export { canvas, ctx };
